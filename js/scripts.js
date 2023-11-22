009e99968b338315ed22203c1c4a3fca65c30cee
Chika Osuagwu <osuagwuchikau@gmail.com>
2023-11-22 12:14:10 -0800

    NPM Scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 /* When the user clicks on a nav-item, scroll to the nav-item content */
-let myScroll = document.getElementById("scrollBtn");
-
 function projectFunction() {
   window.scrollTo(0, 1350);
 }
@@ -15,10 +13,7 @@ function contactFunction() {
 
 /* When the user scrolls down 500px from the top of the page, show the scroll button */
 window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
+  const myScroll = document.querySelector('#scrollBtn');
   if (
     document.body.scrollTop > 500 ||
     document.documentElement.scrollTop > 500
@@ -27,13 +22,14 @@ function scrollFunction() {
   } else {
     myScroll.style.display = "none";
   }
-}
+};
+
 
 /* When the user clicks on the button, scroll to the top of the page */
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-}
+} 
 
 /* Collapse navbar when nav-item is clicked */
 $(".navbar-nav>li>a").on("click", function () {
